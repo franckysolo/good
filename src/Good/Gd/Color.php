767d6b033a1eab8003809afe0f98fe581d1f6706
franckysolo <franckysolo@gmail.com>
2012-10-08 18:15:01 +0200

    Color package

---
+++ src/Good/Gd/Color.php
@@ -0,0 +1,336 @@
+<?php
+namespace Good\Gd;
+/**
+ *  Good 1.0
+ *
+ * @author franckysolo <franckysolo@gmail.com>
+ * @since 26 sept. 2012
+ * @license license.txt
+ * @category Good 
+ * @package Gd
+ * @subpackage 
+ * @filesource Color.php
+ * @version $Id: $
+ * @desc : the color class
+ */
+class Color
+{	
+	/**
+	 * The red channel from 0 to 255
+	 *
+	 * @access protected
+	 * @var integer
+	 */
+	protected $_red;
+	
+	/**
+	 * The blue channel from 0 to 255
+	 *
+	 * @access protected
+	 * @var integer
+	 */
+	protected $_blue;
+	
+	/**
+	 * The blue channel from 0 to 255
+	 *
+	 * @access protected
+	 * @var integer
+	 */
+	protected $_green;
+	
+	/**
+	 * The alpha channel from 0 to 127
+	 *
+	 * @access protected
+	 * @var integer
+	 */
+	protected $_alpha;
+	
+	/**
+	 * The resource
+	 *
+	 * @access protected
+	 * @var gd resource
+	 */
+	protected $_resource = null;
+	
+	/**
+	 * The color index allocation
+	 *
+	 * @access protected
+	 * @var integer
+	 */
+	protected $_index;
+	
+	/**
+	 * Create a new color
+	 *
+	 * @access public
+	 * @param mixed (integer | string | array | arguments) $arguments
+	 */
+	public function __construct($arguments = null)
+	{
+		if(null == $arguments) {
+			$arguments = array(0, 0, 0);
+		}
+	
+		if(!is_array($arguments)) {
+			if(is_string($arguments)) {
+				$arguments = $this->setRgbaFromString($arguments);
+			} else if (is_int($arguments)) {
+				$arguments = $this->setRgbaFromInteger($arguments);				
+			} else {
+				$arguments = func_get_args();
+			}
+		}
+	
+		$this->setRgba($arguments);
+	}
+	
+	/**
+	 * Set the rgba colors
+	 *
+	 * @access public
+	 * @param array $colors
+	 * @throws \InvalidArgumentException
+	 * @return \Good\Gd\Color
+	 */
+	public function setRgba(array $colors = array()) {
+	
+		$count = count($colors);
+	
+		if($count == 3) {
+			$alpha = 0;
+			list($red, $green, $blue) = $colors;
+		} else if($count == 4) {
+			list($red, $green, $blue, $alpha) = $colors;
+		} else {
+			throw new \InvalidArgumentException('3 parameters expected for rgb color, 4 for rgba color', 400);
+		}
+	
+		$this->setRed($red);
+		$this->setGreen($green);
+		$this->setBlue($blue);
+		$this->setAlpha($alpha);
+	
+		return $this;
+	}
+	
+	/**
+	 * Set the the rgba colors from string parameters
+	 *
+	 * @access public
+	 * @param string $stringColor
+	 * @return array
+	 */
+	public function setRgbaFromString($stringColor)
+	{	
+		if(strpos($stringColor, '#') !== false) {
+			ltrim($stringColor, '#');
+		}
+	
+		$red 	= hexdec(substr($stringColor, 0, 2));
+		$green 	= hexdec(substr($stringColor, 2, 2));
+		$blue 	= hexdec(substr($stringColor, 4, 2));
+		$alpha 	= (strlen($stringColor) == 8) ? hexdec(substr($stringColor, 6, 2)) : 0;
+		
+		return array($red, $green, $blue, $alpha);
+	}
+	
+	/**
+	 * Set the the rgba colors from integer parameter
+	 *
+	 * @access public
+	 * @param integer $integer
+	 * @return array
+	 */	
+	public function setRgbaFromInteger($integer)
+	{	
+		$red 	= ($integer >> 16) & 0xff;
+		$green 	= ($integer >> 8) & 0xff;
+		$blue 	= ($integer) & 0xff;
+		$alpha 	= ($integer >> 24 )& 0xff;
+				
+		return array($red, $green, $blue, $alpha);
+	}
+	
+	/**
+	 * Get the rgba color array
+	 *
+	 * @access public
+	 * @return array
+	 */
+	public function getRgba()
+	{
+		return array(
+			$this->_red, $this->_green, $this->_blue, $this->_alpha
+		);
+	}
+	
+	/**
+	 * Set the red channel
+	 *
+	 * @access public
+	 * @param integer $red
+	 * @return \Good\Gd\Color
+	 */
+	public function setRed($red)
+	{
+		if($red < 0 or $red > 255){
+			$red = 127;
+		}
+	
+		$this->_red = (int)$red;
+	
+		return $this;
+	}
+	
+	/**
+	 * Get the red channel
+	 *
+	 * @access public
+	 * @return integer
+	 */
+	public function getRed()
+	{
+		return $this->_red;
+	}
+	
+	/**
+	 * Set the blue channel
+	 *
+	 * @access public
+	 * @param integer $blue
+	 * @return \Good\Gd\Color
+	 */
+	public function setBlue($blue)
+	{
+		if($blue < 0 or $blue > 255){
+			$blue = 127;
+		}
+	
+		$this->_blue = (int)$blue;
+	
+		return $this;
+	}
+	
+	/**
+	 * Get the blue channel
+	 *
+	 * @access public
+	 * @return integer
+	 */
+	public function getBlue()
+	{
+		return $this->_blue;
+	}
+	
+	/**
+	 * Set the green channel
+	 *
+	 * @access public
+	 * @param integer $green
+	 * @return \Good\Gd\Color
+	 */
+	public function setGreen($green)
+	{
+		if($green < 0 or $green > 255){
+			$green = 127;
+		}
+	
+		$this->_green = (int)$green;
+	
+		return $this;
+	}
+	
+	/**
+	 * Get the green channel
+	 *
+	 * @access public
+	 * @return integer
+	 */
+	public function getGreen()
+	{
+		return $this->_green;
+	}
+	
+	/**
+	 * Set the alpha channel
+	 *
+	 * @access public
+	 * @param integer $alpha
+	 * @return \Good\Gd\Color
+	 */
+	public function setAlpha($alpha)
+	{
+		if($alpha < 0 or $alpha > 127){
+			$alpha = 127;
+		}
+	
+		$this->_alpha = (int)$alpha;
+	
+		return $this;
+	}
+	
+	/**
+	 * Get the alpha channel
+	 * 
+	 * @access public
+	 * @return integer
+	 */
+	public function getAlpha()
+	{
+		return $this->_alpha;
+	}
+	
+	/**
+	 * Allocate the gd color
+	 *
+	 * @access public
+	 * @param gd resource $resource
+	 * @throws \RuntimeException
+	 * @return \Good\Gd\Color
+	 */
+	public function allocate($resource)
+	{
+		if($this->_alpha == 0) {
+			$index = imagecolorallocate($resource, $this->_red, $this->_green, $this->_blue);
+		} else {
+			$index = imagecolorallocatealpha($resource, $this->_red, $this->_green, $this->_blue, $this->_alpha);
+		}
+	
+		if(false === $index) {
+			throw new \RuntimeException('Unable to allocate color', 500);
+		}
+	
+		$this->_index = $index;
+		$this->_resource = $resource;
+
+		return $this;
+	}
+	
+	/**
+	 * Get the index color allocation
+	 *
+	 * @access public
+	 * @return integer
+	 */
+	public function getIndex()
+	{
+		return $this->_index;
+	}
+	
+	/**
+	 * Unset colors allocation
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __destruct()
+	{
+		if(false !== $this->_index and is_resource($this->_resource)) {
+			imagecolordeallocate($this->_resource, $this->_index);
+		}
+	}
+}
---
+++ src/Good/Gd/Color/Palette.php
@@ -0,0 +1,187 @@
+<?php
+namespace Good\Gd\Color;
+/** 
+ *  Good 1.0
+ *
+ * @author franckysolo <franckysolo@gmail.com>
+ * @since 28 sept. 2012
+ * @license license.txt
+ * @category Good 
+ * @package Gd
+ * @subpackage Color
+ * @filesource Palette.php
+ * @version $Id: $
+ * @desc : the palette color class
+ */
+class Palette 
+{
+	/**
+	 * Some palette constante
+	 * 
+	 * @access public
+	 * @var integer
+	 */
+	const TRANSPARENT 	= 0x7fffffff;
+	const WHITE 	  	= 0xffffff;
+	const LIGHTGRAY 	= 0xececec;
+	const DARKGRAY 		= 0x666666;
+	const SILVER 		= 0xc0c0c0;
+	const GRAY 			= 0x999999;
+	const MAROON		= 0x800000;
+	const BLUE 			= 0x0000ff;	
+	const PURPLE 		= 0x800080;
+	const FUSHIA		= 0xff00ff;
+	const NAVY			= 0x000080;
+	const AQUA			= 0x00ffff;
+	const TEAL			= 0x008080;
+	const RED 			= 0xff0000;
+	const LIME 			= 0x00ff00;
+	const GREEN 		= 0x008000;
+	const OLIVE 		= 0x808000;
+	const YELLOW 		= 0x00ffff;
+	const ORANGE 		= 0xffcc00;	
+	const BLACK 	  	= 0x000000;
+	
+	/**
+	 * The pallette colors
+	 * 
+	 * @access protected
+	 * @var array
+	 */
+	protected $_colors = array();
+	
+	/**
+	 * Set the colors
+	 * 
+	 * @access public
+	 * @param array $colors
+	 * @throws \InvalidArgumentException
+	 * @return \Good\Gd\Color\Palette
+	 */
+	public function setColors(array $colors = array())
+	{
+		foreach($colors as $key => $color) {
+			if(!is_string($name)) {
+				$message = 'String expected for color key';
+				throw new \InvalidArgumentException($message, 500);
+			}
+			
+			$this->_colors[(string)$key] = $color;
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * Returns the palette colors array
+	 * 
+	 * @access public
+	 * @return array
+	 */
+	public function getColors()
+	{
+		return $this->_colors;
+	}
+	
+	/**
+	 * Set random colors array pair key (value in hexa) value (value in integer)
+	 * 
+	 * @access public
+	 * @param integer $numberOfColor
+	 * @param boolean $randomAlpha
+	 * @return array  
+	 */
+	public function randomColor($numberOfColor, $randomAlpha = false)
+	{
+		$colors = array();
+	
+		for($i = 0; $i < $numberOfColor; ++$i) {
+				
+			$red 	= mt_rand(0, 255);
+			$green 	= mt_rand(0, 255);
+			$blue 	= mt_rand(0, 255);
+			$color 	= array($red, $green, $blue);
+			
+			if($randomAlpha) {
+				$alpha 	=  mt_rand(0, 127);
+				array_push($color, $alpha);
+			}
+						
+			$key = $this->rgbaToHtml($color);
+			$colors[$key] = $color;		
+		}
+		
+		return $colors;
+	}
+	
+	/**
+	 * Convert rgb value tohtml format
+	 * 
+	 * @access public
+	 * @param array $colors
+	 * @return string
+	 */
+	public function rgbaToHtml(array $colors = array())
+	{
+		$html = '#';
+		foreach ($colors as $value) {
+			$html .= $this->rgbToHexa($value);
+		}
+		
+		return $html;
+	}
+	
+	/**
+	 * Convert rgb value hexadecimal 
+	 * 
+	 * @access public
+	 * @param integer $number
+	 * @return string
+	 */
+	public function rgbToHexa($number)
+	{
+		if($number < 0) {
+			$number = 0;
+		} else if ($number > 255) {
+			$number = 255;
+		}
+	
+		$html = dechex($number);
+		$html = (strlen($html) == 2) ? $html : '0' . $html;
+	
+		return $html;
+	}
+	
+	/**
+	 * Returns a parameter
+	 * 
+	 * @access public
+	 * @param string $name
+	 * @return string
+	 */
+	public function __get($name) {
+		if(isset($this->_colors[$name])) {
+			return $this->_colors[$name];
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * Set a parameter
+	 * 
+	 * @access public
+	 * @param string $name
+	 * @param integer $value
+	 * @throws \InvalidArgumentException
+	 * @return void
+	 */
+	public function __set($name, $value) {
+		if(!is_string($name)) {
+			$message = 'String expected for color key';
+			throw new \InvalidArgumentException($message, 500);
+		}
+			
+		$this->_colors[(string)$name] = $value;
+	}	
+}
