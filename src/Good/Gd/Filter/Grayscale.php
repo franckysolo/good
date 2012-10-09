cbb000321fb300a16f14c717ccc93c6ea6f2c6e1
franckysolo <franckysolo@gmail.com>
2012-10-09 17:08:07 +0200

    Filter package

---
+++ src/Good/Gd/Filter/Grayscale.php
@@ -0,0 +1,29 @@
+<?php
+namespace Good\Gd\Filter;
+use Good\Gd\Filter;
+/** 
+ *  Phpmedias 1.0
+ *
+ * @author franckysolo <franckysolo@gmail.com>
+ * @since 27 sept. 2012
+ * @license license.txt
+ * @category Phpmedias 
+ * @package
+ * @subpackage
+ * @filesource Grayscale.php
+ * @version $Id: $
+ * @desc :
+ */
+class Grayscale extends Filter
+{
+	/**
+	 * (non-PHPdoc)
+	 * @see Good\Gd.Filter::apply()
+	 */
+	public function apply($resource)
+	{
+		if(!imagefilter($resource, IMG_FILTER_GRAYSCALE)) {
+			throw \RuntimeException('Unable to apply gaussian blur filter');
+		}
+	}
+}
