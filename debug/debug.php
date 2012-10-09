97003af2ab12f38593b6d0bae236e4e103ef59be
franckysolo <franckysolo@gmail.com>
2012-10-09 17:12:09 +0200

    debug file

--- debug/debug.php
+++ debug/debug.php
@@ -7,7 +7,32 @@ use Good\Core\View;
 Autoloader::start();
 
 $serie = new Serie(array(
-	'tests\Codec\GifCase',
+		
+	//'tests\Codec\GifCase',
+	//'tests\Codec\PngCase',
+
+	//'tests\Color\PaletteCase',
+	//'tests\Color\ColorCase',
+
+	//'tests\Fractals\BudhaCase',
+	//'tests\Fractals\JuliaCase',
+	//'tests\Fractals\MandelCase',
+				
+	//'tests\Patterns\SimpleDrawingCase',
+	//'tests\Patterns\PolygonDrawingCase',
+	//'tests\Patterns\TextDrawingCase',
+	//'tests\Patterns\RoundedDrawingCase',
+	//'tests\Patterns\DashedLineDrawingCase',
+
+ 	//'tests\Transformation\SimpleRotationCase',
+ 	//'tests\Transformation\RotationCase',
+ 	//'tests\Transformation\ComplexRotationCase',
+	//'tests\Transformation\ThumbnailCase',
+ 	'tests\Transformation\CropCase',
+//	'tests\Transformation\MergeCase',
+	
 ));
 
 View::newInstance()->render($serie->run());
+
+Autoloader::stop();
