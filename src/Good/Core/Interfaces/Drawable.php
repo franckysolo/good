b8ded230147886fd3f2817253c0026b8668613a1
franckysolo <franckysolo@gmail.com>
2012-10-20 19:03:45 +0200

    Interfaces - add php doc

--- src/Good/Core/Interfaces/Drawable.php
+++ src/Good/Core/Interfaces/Drawable.php
@@ -1,6 +1,27 @@
 <?php
+/**
+ * Good 1.0 (Gif oriented object drawing)
+ *
+ * @author franckysolo
+ */
 namespace Good\Core\Interfaces;
+/**
+ *  Good 1.0
+ *
+ * @author franckysolo <franckysolo@gmail.com>
+ * @since 20 oct. 2012
+ * @license license.txt
+ * @category Good 
+ * @package Core
+ * @subpackage Interfaces
+ * @filesource Drawable.php
+ * @version $Id: $
+ * @desc : the Drawable interface
+ */
 interface Drawable
 {
+	/**
+	 * Drawing some element
+	 */
 	public function draw();
 }
--- src/Good/Core/Interfaces/Transformable.php
+++ src/Good/Core/Interfaces/Transformable.php
@@ -1,6 +1,27 @@
 <?php
+/**
+ *  Good 1.0
+ *
+ * @author franckysolo <franckysolo@gmail.com>
+ */
 namespace Good\Core\Interfaces;
+ /**
+ *  Good 1.0
+ *
+ * @author franckysolo <franckysolo@gmail.com>
+ * @since 20 oct. 2012
+ * @license license.txt
+ * @category Good 
+ * @package
+ * @subpackage
+ * @filesource Transformable.php
+ * @version $Id: $
+ * @desc : the Transformable interface
+ */
 interface Transformable
 {
+	/**
+	 * Execute a transformation
+	 */
 	public function execute();
 }
