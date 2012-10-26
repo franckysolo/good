cbd4ff2c2abb9b2db75d932181ceb4c3e42c5630
franckysolo <franckysolo@gmail.com>
2012-10-26 18:06:36 +0200

    Chart Package

---
+++ src/Good/Chart/Chart.php
@@ -0,0 +1,83 @@
+<?php
+namespace Good\Chart;
+use Good\Chart\Util\Position;
+use Good\Chart\Element\Label;
+use Good\Gd\Image;
+use Good\Gd\Pattern\Rectangle;
+use Good\Gd\Pattern\Fill;
+use Good\Gd\Color\Palette;
+class Chart extends Image
+{
+	/**
+	 * The title of chart
+	 * 
+	 * @access protected
+	 * @var string  
+	 */
+	protected $_tilte;
+	
+	protected $_plot;
+	
+	/**
+	 * Create a new chart
+	 * 
+	 * @param integer $width
+	 * @param integer $height
+	 */
+	public function __construct($width = 400, $height = 300)
+	{
+		parent::__construct($width, $height);
+		$this->newLayer('background');
+	}
+	
+	public function setBorder($color = Palette::BLACK)
+	{
+		list($w, $h) = $this->getCanvasSize();
+	
+		$rectangle = new Rectangle($this->getLayer()->getResource());
+		$rectangle->setColor($color)
+				  ->setCoordinates(0, 0, $w - 1, $h - 1)
+				  ->draw();
+	
+		return $this;
+	}
+	
+	public function setBackgroundColor($color = Palette::LIGHTGRAY)
+	{
+		$fill = new Fill($this->getLayer()->getResource());
+		$fill->setColor($color)
+			 ->setCoordinates(0, 0)
+			 ->draw();
+	
+		return $this;
+	}
+	
+	public function setTitle($title, $size = 10, $angle = 0)
+	{
+		list($w, $h) = $this->getCanvasSize();
+		
+		$label = new Label($this->getLayer()->getResource());
+		$label->setText($title)
+			  ->setAngle($angle)
+			  ->setSize($size)
+			  ->alignText($w, $h)
+			  ->draw();
+		
+		$this->_title = $title;
+		
+		return $this;
+	}
+	
+	
+	public function plot(Plot $plot)
+	{
+		$this->_plot = $plot;
+		$this->_plot->init();
+	}
+	
+	public function generate()
+	{
+		$this->_plot->draw();
+		$this->save($this->_plot->getName());
+	}
+}
---
+++ src/Good/Chart/Data.php
@@ -0,0 +1,153 @@
+<?php
+namespace Good\Chart;
+
+class Data implements \Countable
+{
+	/**
+	 *
+	 * Enter description here ...
+	 * @var array
+	 */
+	protected $_datax = array();
+	
+	/**
+	 *
+	 * Enter description here ...
+	 * @var array
+	 */
+	protected $_datay = array();
+	
+	/**
+	 * 
+	 * @param array $datay
+	 * @param array $datax
+	 * @throws \OutOfBoundsException
+	 */
+	public function __construct(array $datay, $datax = null)
+	{
+	
+		foreach ($datay as $key => $data) {
+			$this->_datay[] = $data;
+		}
+	
+		$county = count($this->_datay);
+	
+		if (null === $datax) {
+			for ($i = 0; $i < $county; $i++) {
+				$this->_datax[] = $i;
+			}
+		} else {
+			$this->_datax = $datax;
+		}
+	
+		$countx = count($this->_datax);
+
+		if ($countx != $county) {
+			$message = sprintf('Same count expected for datay & datax, %d versus %d', $county, $countx);
+			throw new \OutOfBoundsException($message);
+		}
+	}
+	
+	/**
+	 *
+	 * Enter description here ...
+	 * @return mixed
+	 */
+	public function xmin()
+	{
+		return min($this->_datax);
+	}
+	/**
+	 *
+	 * Enter description here ...
+	 * @return mixed
+	 */
+	public function xmax()
+	{
+		return max($this->_datax);
+	}
+	
+	/**
+	 *
+	 * Enter description here ...
+	 * @return integer
+	 */
+	public function xRange()
+	{
+		return $this->xmax() + 1;
+	}
+	/**
+	 *
+	 * Enter description here ...
+	 * @return integer
+	 */
+	public function yRange()
+	{
+		$min = ($this->ymin() > 0) ? 0 : $this->ymin();
+		return $this->ymax() - $min + 1;
+	}
+	/**
+	 *
+	 * Enter description here ...
+	 * @return mixed
+	 */
+	public function ymin()
+	{
+		return min($this->_datay);
+	}
+	
+	/**
+	 *
+	 * Enter description here ...
+	 * @return mixed
+	 */
+	public function ymax()
+	{
+		return max($this->_datay);
+	}
+	
+	/**
+	 * Retourne la serie de points de l'axe des x
+	 * @return array
+	 */
+	public function getDatax()
+	{
+		return $this->_datax;
+	}
+	/**
+	 * Retourne la serie de points de l'axe des y
+	 * @return array
+	 */
+	public function getDatay()
+	{
+		return $this->_datay;
+	}
+	
+	/**
+	 *
+	 * Enter description here ...
+	 * @return integer
+	 */
+	public function xsum()
+	{
+		return array_sum($this->_datax);
+	}
+	/**
+	 *
+	 * Enter description here ...
+	 * @return integer
+	 */
+	public function ysum()
+	{
+		return array_sum($this->_datay);
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Countable::count()
+	 */
+	public function count()
+	{
+		return count($this->_datay);
+	}
+}
